3a1cfc6c28e34e5793cd87bb6504e76d6b6d1937
aoka <aoka.555.ibiza@gmail.com>
2021-04-04 17:00:51 +0900

    FirstCommit

---
+++ aoka-TaskApp7/GreenViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GreenViewController.swift
+//  aoka-TaskApp7
+//
+//  Created by aoka on 2021/04/04.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    @IBOutlet private weak var textField: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    
+    @IBOutlet private weak var answerLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textField.keyboardType = .numberPad
+        textField2.keyboardType = .numberPad
+    }
+    
+    @IBAction func answerButton(_ sender: Any) {
+        let number = Int(textField.text!) ?? 0
+        let number2 = Int(textField2.text!) ?? 0
+        
+        answerLabel.text = String(number - number2)
+        
+        textField.endEditing(true)
+        textField2.endEditing(true)
+    }
+    
+}
